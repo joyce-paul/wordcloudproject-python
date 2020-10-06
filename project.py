aeaa818c534a6fed2f901bbbeb6007c779c53791
joyce-paul <joyce_paul@hotmail.com>
2020-10-06 16:17:22 +0800

    Revised code pertaining to colour, font

--- project.py
+++ project.py
@@ -3,10 +3,10 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 # List of words for the wordcloud
-text=("mySkills mySkills mySkills HTML HTML CSS CSS Coding Python JavaScript SQL Matplotlib Pandas Visualization Visualization pageLayout inDesign Dataviz Wordcloud Barcharts Writing Writing Editing Editing projectManagement")
+text=("mySkills mySkills mySkills mySkills HTML HTML HTML CSS CSS CSS Coding Python Python JavaScript SQL Matplotlib Pandas Visualization Visualization pageLayout inDesign Dataviz Wordcloud Barcharts Writing Writing Editing Editing projectManagement")
 
 # Wordcloud object
-wordcloud = WordCloud(width=540, height=540, margin=0).generate(text)
+wordcloud = WordCloud(width=540, height=540, margin=0, background_color="orange", max_words=12, colormap="Blues").generate(text)
 
 # Display the generated image:
 plt.imshow(wordcloud, interpolation='bilinear')
